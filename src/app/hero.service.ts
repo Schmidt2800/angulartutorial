196712f05ab57a2dad89399fc09d93b26ec6d347
rsc <rsc@unik.dk>
2021-11-15 15:45:38 +0100

    the end

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -39,6 +39,7 @@ export class HeroService {
         catchError(this.handleError<any>('updateHero'))
       );
   }
+
   addHero(hero: Hero): Observable<Hero> {
     return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
       tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
@@ -54,6 +55,19 @@ export class HeroService {
       catchError(this.handleError<Hero>('deleteHero'))
     );
   }
+
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(x => x.length ?
+        this.log(`found heroes matching "${term}"`):
+        this.log(`no heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
+  
   private log(message: string)
   {
     this.messageService.add(`HeroService: ${message}`);
